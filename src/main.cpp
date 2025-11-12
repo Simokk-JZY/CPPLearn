7e080a74c429ed349d3196ce33fd1f42b0964a56
unknown <simokk>
2025-11-12 16:53:58 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "stdafx.h"
 
-int main()
-{
+int main() {
     long  nErr, nPort;//定义端口变量
     AmsAddr Addr;
     PAmsAddr pAddr = &Addr;
-    UINT control1, tesx;
+    unsigned short stateW1;
+    unsigned short control1[]={0,6,7,15};
+    int keyinput = 1;
 
     // 在ADS路由器上打开通信端口
     nPort = AdsPortOpen();
@@ -16,26 +17,43 @@ int main()
     pAddr->port = 851;
 
     // 从用户处读取需写入PLC的数值
-    do {
-        std::cin >> tesx;
-        nErr = AdsSyncWriteReq(pAddr,0xF030,0x3E800,0x2, &tesx );
+    for (int i=1 ;i < 4; i++ )
+     {
+        // std::cin >> control1;
+        // 将数值写入寄存器
+        nErr = AdsSyncWriteReq(pAddr,0xF030,0x3E800,0x2, &(control1[i]) );
         if (nErr) std::cerr << "Error: AdsSyncWriteReq: " << nErr << '\n';
-        nErr = AdsSyncReadReq(pAddr,0xF030,0x3E800,0x2, &control1 );
+        // std::cout << "Error: AdsSyncWriteReq: " << nErr << '\n'；
+        //从寄存器读取数值
+        nErr = AdsSyncReadReq(pAddr,0xF020,0x1F400,0x2, &stateW1 );
         if (nErr) std::cerr << "Error: AdsSyncReadReq: " << nErr << '\n';
-        std::cout << control1 << '\n';
-    }while (getchar() =='\n');
+        std::cout << stateW1 << '\n';
+        Sleep(50);
+    }
+    while (keyinput) {
+        char c=getchar();
+        if (c == '\n') keyinput = 0;
+    }
+    keyinput = 1;
 
-    // 将数值写入MD0寄存器
-    // nErr = AdsSyncWriteReq( pAddr, 0x4020, 0x0, 0x4, & );
-    // if (nErr) std::cerr << "Error: AdsSyncWriteReq: " << nErr << '\n';
+    for (int i=2 ; i>-1 ; i-- )
+    {
+        // std::cin >> control1;
+        nErr = AdsSyncWriteReq(pAddr,0xF030,0x3E800,0x2, &(control1[i]) );
+        if (nErr) std::cerr << "Error: AdsSyncWriteReq: " << nErr << '\n';
+        // std::cout << "Error: AdsSyncWriteReq: " << nErr << '\n'；
+        nErr = AdsSyncReadReq(pAddr,0xF020,0x1F400,0x2, &stateW1 );
+        if (nErr) std::cerr << "Error: AdsSyncReadReq: " << nErr << '\n';
+        std::cout << stateW1 << '\n';
+        Sleep(50);
+    }
+
+
 
     // 关闭通信端口
     nErr = AdsPortClose();
     if (nErr) std::cerr << "Error: AdsPortClose: " << nErr << '\n';
 
     return 0;
-
-
-
 
 }
