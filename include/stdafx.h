5322e220ba79924e1f096a082ff8d7e2a037cac2
unknown <simokk>
2025-11-14 16:57:43 +0800

    update

--- include/stdafx.h
+++ include/stdafx.h
@@ -9,8 +9,9 @@
 #include <iostream> //标准的输入输出流头文件
 #include <windows.h> //windowsAPI的函数声明和宏
 #include <conio.h> //控制台输入输出头文件
+#include <bitset> //std::bitset<size>(num)
 // ADS headers for TwinCAT 3
 #include "TcAdsDef.h" //结构体和常量的声明
 #include "TcAdsAPI.h" // ADS函数的声明
-#include "minwindef.h"
+#include "minwindef.h"//使用简化声明
 #endif //CPPLEARN_STDAFX_H
